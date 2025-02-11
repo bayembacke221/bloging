92f57197e6470aac0ab6bd8dca1759e64dab98b3
Mbacke Mbaye <mbackembaye74@gmail.com>
2025-02-11 15:50:26 +0000

    configgutation tailwindcss,alpinejs and vite

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const  defaultTheme = require('tailwindcss/defaultTheme');
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+    ],
+    theme: {
+        extend: {
+            fontFamily:{
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            }
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms')({
+            strategy: 'class',
+        }),
+    ],
+}
